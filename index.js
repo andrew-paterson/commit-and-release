012515c9c23645a89f3333a77e568030fa5ab150
andrew-paterson <andrew.paterson9@gmail.com>
2021-07-02 11:09:03 +0200

    Commit 6

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const simpleGit = require('simple-git');
 const options = {
   // baseDir: '../pandago'
 }
-const commitMessage = 'Commit 5';
-const tagMessage = 'tag message 5'
+const commitMessage = 'Commit 6';
+const tagMessage = 'tag message 6'
 const git = simpleGit(options);
 
 async function run() {
@@ -19,6 +19,7 @@ async function run() {
   const tagArgs = tagMessage ? ['-a', newTag, '-m', tagMessage] : [newTag];
   await git.tag(tagArgs, function(err, result) {
     console.log('result');
+    console.log(err)
     console.log(result);
   });
   // console.log(await git.tag());
