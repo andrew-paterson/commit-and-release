2e1fedde3501cf992312069f89d51251ee899b4d
andrew-paterson <andrew.paterson9@gmail.com>
2021-07-02 11:27:02 +0200

    Commit 6

--- index.js
+++ index.js
@@ -8,22 +8,16 @@ const tagMessage = 'tag message 6'
 const git = simpleGit(options);
 
 async function run() {
-  // console.log(await git.status());
-
   await git.add('.');
- 
   await git.commit(commitMessage);
-  // console.log(await git.push());
   const newTag = await lib.bumpTag(git);
-  // console.log(newTag);
   const tagArgs = tagMessage ? ['-a', newTag, '-m', tagMessage] : [newTag];
   await git.tag(tagArgs);
   const showTag = await git.show(newTag);
   const newtagCommit = showTag.split('\n').filter(line => line.startsWith('commit'));
   console.log(`Added new ${showTag.split('\n')[0]} to ${newtagCommit}`);
-  // console.log(await lib.highestTag(git));
-
-  // console.log(await git.tag());
+  console.log(await git.push());
+  console.log(await git.push(['--tags']));
 }
 
 run();
