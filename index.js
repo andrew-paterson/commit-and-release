8b0aec95489d094a1e4142c46aa668c7e0da27e4
andrew-paterson <andrew.paterson9@gmail.com>
2021-07-02 11:00:53 +0200

    Commit 1

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const options = {
   // baseDir: '../pandago'
 }
 const commitMessage = 'Commit 1';
+const tagMessage = 'tag message'
 const git = simpleGit(options);
 
 async function run() {
-  console.log(await git.status());
+  // console.log(await git.status());
 
   await git.add('.');
+ 
+  await git.commit(commitMessage);
+  // console.log(await git.push());
   const newTag = await lib.bumpTag(git, {releaseType:'major'});
   console.log(newTag);
-  await git.commit(commitMessage);
-  console.log(await git.push());
+  const tagArgs = tagMessage ? ['-a', newTag, '-m', tagMessage] : [newTag];
+  await git.tag(tagArgs);
+  console.log(await git.tag());
 }
 
 run();
